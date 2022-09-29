781477dab9a7b29a395a61b4e95cfc1015e90623
fabian llanten <fabianandresl10@gmail.com>
2022-09-28 21:07:33 -0500

    login y servicios

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+
+const routes: Routes = [
+{
+  path:'',
+  component: InicioComponent
+},
+{
+  path:'auth',
+  loadChildren:() => import('./auth/auth.module').then(m =>m.AuthModule)
+},
+{
+  path:'admin',
+  loadChildren:() => import('./admin/admin.module').then(m =>m.AdminModule)
+}
+
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
